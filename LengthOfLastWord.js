73f28fea418ced8e0dd75c31cba28ef8bc901e45
BimalTimsina <bimaltimsina4@gmail.com>
2024-05-13 23:29:58 +0545

    length of last word solved in more optimized way

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -13,3 +13,20 @@ var lengthOfLastWord = function (s) {
 
 const output = lengthOfLastWord("My name is Bimal     ");
 console.log(output);
+
+//next way(more optimized)
+var lengthOfLastWord = function (s) {
+  let count = 0;
+  let store = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      count = 0;
+    } else {
+      count++;
+      store = count;
+    }
+  }
+  return store;
+};
+const ans = lengthOfLastWord("hgjka agakgk agklak lg  ");
+console.log(ans);
